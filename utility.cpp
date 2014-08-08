524ce76019864a2b2f2bacd33adade1e4baa85bf
Tianjun <tianjunyeye@gmail.com>
2014-08-08 17:26:57 +0800

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -18,23 +18,23 @@ void Write(int frameid, int x, int y) {
 
 void initialize(vector<vector<int> >& A) {
     for(int i=0; i<DBMAX; i++) {
-	    A[i][0] = random()%ValueLimit;
-	    A[i][1] = random()%ValueLimit;
-	    A[i][2] = i;
+	A[i][0] = random()%ValueLimit;
+	A[i][1] = random()%ValueLimit;
+	A[i][2] = i;
     }
 }
 
 int Pattern1(int frameid) {
-	int num = random()%10;
-	if(num<8) {
-		cout << "same frameid as previous " << frameid << endl;
-	    return frameid;
-	}
-	else {
-		int newid=random()%DBMAX;
-		cout << "frameID " << newid<<endl; 
-	    return newid;
-	 }
+    int num = random()%10;
+    if(num<8) {
+	cout << "same frameid as previous " << frameid << endl;
+	return frameid;
+    }
+    else {
+	int newid=random()%DBMAX;
+	cout << "frameID " << newid<<endl; 
+	return newid;
+    }
 }
 
 void SimulateLFU(int count, int pattern, LFUCache* cache_obj) {
@@ -45,24 +45,23 @@ void SimulateLFU(int count, int pattern, LFUCache* cache_obj) {
     int frameid = random()%DBMAX;
     cout << "frameid " << frameid <<endl;
     int value = 0;
- 
 
     for(int i=0; i<count; i++) {		
-	    if(Read(frameid, value, cache_obj)) 
-		    hit++;
-	    else
-		    miss++;
+	if(Read(frameid, value, cache_obj)) 
+	    hit++;
+	else
+	    miss++;
 		    
-		PrintLFUCacheStatus(cache_obj);
-		cout<< endl << endl;
-	    switch(pattern) {
-	        case 1:
-		        frameid = Pattern1(frameid);
-		        break;
-	        default:
-	            frameid = random()%DBMAX;
-	            break;
-	    }
+	PrintLFUCacheStatus(cache_obj);
+	cout<< endl << endl;
+	switch(pattern) {
+	    case 1:
+		frameid = Pattern1(frameid);
+		break;
+	    default:
+	        frameid = random()%DBMAX;
+	        break;
+	}
     }
 
     cout << "The hitting/missing ratio is: " << double(hit)/double(miss) << endl;
@@ -78,23 +77,22 @@ void SimulateLRU(int count, int pattern, LRUCache* cache_obj) {
     cout << "frameid " << frameid <<endl;
     int value = 0;
  
-
     for(int i=0; i<count; i++) {		
-	    if(Read(frameid, value, cache_obj)) 
-		    hit++;
-	    else
-		    miss++;
+	if(Read(frameid, value, cache_obj)) 
+	    hit++;
+	else
+            miss++;
 		    
-		PrintLRUCacheStatus(cache_obj);
-		cout<< endl << endl;
-	    switch(pattern) {
-	        case 1:
-		        frameid = Pattern1(frameid);
-		        break;
-	        default:
-	            frameid = random()%DBMAX;
-	            break;
-	    }
+	PrintLRUCacheStatus(cache_obj);
+        cout<< endl << endl;
+	switch(pattern) {
+	    case 1:
+		frameid = Pattern1(frameid);
+		break;
+	    default:
+	        frameid = random()%DBMAX;
+	        break;
+	}
     }
     cout << "The hitting/missing ratio is: " << double(hit)/double(miss) << endl;
     return;
@@ -102,7 +100,7 @@ void SimulateLRU(int count, int pattern, LRUCache* cache_obj) {
 
 void PrintLFUCacheStatus(LFUCache* lfucache) {
     FreqNode* freqnode = lfucache->FreqHead->Next;
-
+    
     while (freqnode) {
         cout<<"frequency "<<freqnode->Freq<<":";
         LFUListNode* listnode = freqnode->HeadNode;
@@ -117,12 +115,12 @@ void PrintLFUCacheStatus(LFUCache* lfucache) {
 }
 
 void PrintLRUCacheStatus(LRUCache* lrucache) {
-	LRUListNode* listnode = lrucache->Head;
+    LRUListNode* listnode = lrucache->Head;
 	
-	while(listnode) {
-		cout<<listnode->Value<<", ";
-		if(listnode==lrucache->End) break;
-		listnode = listnode->Next;
+    while(listnode) {
+	cout<<listnode->Value<<", ";
+	if(listnode==lrucache->End) break;
+	listnode = listnode->Next;
     }
     cout<<endl;
 }
